4d01a30beb0869fa7ee13eeeff2cd5eef58f8c70
balde <alrougui767@gmail.com>
2019-02-18 12:31:39 +0100

    commit

---
+++ app/Http/Controllers/ApiNewsLetterController.php
@@ -0,0 +1,17 @@
+
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use App\NewsLetterSubscription;
+class ApiNewsLetterController extends Controller
+{
+    public function list() {
+
+        return NewsLetterSubscription::all();
+    }
+
+    public function show($id) {
+
+        return NewsLetterSubscription::find($id);
+    }
+}
